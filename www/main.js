221547408786b6eb9c2f066ce1f21b9442d7cd9a
“[firstname <rkx927raj@gmail.com>
2026-04-07 04:43:15 +0530

    Added Siri_wave animation while talking to the jarvis

--- www/main.js
+++ www/main.js
@@ -8,3 +8,31 @@ $('.text').textillate({
         effect: "animate__bounceOut"
     }
 });
+
+$(document).ready(function () {
+
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",
+        speed: 0.08,
+        amplitude: 0.7,
+        autostart: true,
+    });
+
+    // siri message animation
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "animate__fadeInUp",
+            sync: true,
+        },
+        out: {
+            effect: "animate__fadeOutUp",
+            sync: true,
+        }
+    });
+
+});
