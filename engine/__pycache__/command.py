5391658ce513cb0252716f043a7c2c70507e7b44
“[firstname <rkx927raj@gmail.com>
2026-04-23 05:16:21 +0530

    Add speech to text recognition function

--- engine/__pycache__/command.py
+++ engine/__pycache__/command.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 engine = pyttsx3.init()
 
 def speak(text):
@@ -10,5 +11,27 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def takecommand():
 
-speak("Hello im Jarvis, how can i help you today is there any thing going in the mind of your ?")
+    r =sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening...')
+        r.pause_thresold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source, 10, 6) # 10sec for the pause and 6 sec take the speech of the user
+    
+    try:
+        print('recogninition')
+        query = r.recognize_google(audio, language='end-in')
+        print(f"user said: {query}")
+
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+text = takecommand()
+
+speak(text)
